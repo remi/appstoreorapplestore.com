7adee5448b2e09ce1a82e8c70d407d15ce75db39
Rémi Prévost <remi@exomel.com>
2016-09-24 20:26:51 -0400

    Add Let’s Encrypt verification

--- app.rb
+++ app.rb
@@ -17,6 +17,10 @@ class App < Sinatra::Base
     set :haml, settings.haml.merge(:ugly => true)
   end
 
+  get '/.well-known/acme-challenge/RrwlnNejsKEMl4xx8Q6-sA4iHoj2X1UhTb9oHMs4K_I' do
+    "RrwlnNejsKEMl4xx8Q6-sA4iHoj2X1UhTb9oHMs4K_I.fpnoueQ49h7Hmk0d8htnUcSxeCUKrDOCfLOVUoh0ZJ4"
+  end
+
   get "/" do
     haml :index
   end
